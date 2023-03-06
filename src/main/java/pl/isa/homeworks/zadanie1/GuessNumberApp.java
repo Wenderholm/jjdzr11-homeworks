4d7570b05bc6c115d8b08a11032b7a24830a8146
“przemek <pwenderholm@gmail.com>
2023-03-06 20:41:44 +0100

    rozwiązdanie zdania 1

--- src/main/java/pl/isa/homeworks/zadanie1/GuessNumberApp.java
+++ src/main/java/pl/isa/homeworks/zadanie1/GuessNumberApp.java
@@ -8,13 +8,54 @@ public class GuessNumberApp {
         Scanner input = new Scanner(System.in);
 
         System.out.println(game.getIntro());
-
+        System.out.println("wybierz tryb gry wpisujac: STANDARD lub PLAYER");
+        String gameMode = input.nextLine();
+// PLAYER GAME MODE
+        if (gameMode.equalsIgnoreCase("player")) {
+            playerModeGame(game, input);
+        }
+// STANDARD GAME MODE
         while (game.isRunning()) {
             System.out.println(game.getInstruction());
-
-            int number = input.nextInt();
-
+            int number = checkCorrectInput(input);
             System.out.println(game.guessNumber(number));
         }
+    }
+
+    private static void playerModeGame(GuessNumberGame game, Scanner input) {
+        System.out.println("podaj pirwsza liczbę zakresu");
+        int min = checkCorrectInput(input);
+
+        System.out.println("podaj drugą liczbę zakresu");
+        int max = checkCorrectInput(input);
+        if(max < min){
+            System.out.println("podaj druga liczbę większą niż pierwsza");
+            max = checkCorrectInput(input);
+        }
+        game.playerNumberToGuess(min, max);
+
+        System.out.println("ile chcesz mieć szans na odgadnięcie");
+        int numberOfChances = input.nextInt();
+
+        while (game.isRunning()) {
+            System.out.println(game.getPlayerInstruction(min, max));
+            System.out.println("Masz " +  numberOfChances + " szans");
+            int number = checkCorrectInput(input);
+            numberOfChances--;
+            System.out.println(game.guessPlayerNumber(number, numberOfChances));
+        }
+    }
+
+    //    metoda sprawdza czy wprowadzone dane z Scannera są int
+    private static int checkCorrectInput(Scanner input) {
+        int number;
+        do {
+            while (!input.hasNextInt()) {
+                System.out.println("To nie jest liczba");
+                input.next();
+            }
+            number = input.nextInt();
+        } while (number <= 0);
+        return number;
     }
 }
--- src/main/java/pl/isa/homeworks/zadanie1/GuessNumberGame.java
+++ src/main/java/pl/isa/homeworks/zadanie1/GuessNumberGame.java
@@ -1,21 +1,74 @@
 package pl.isa.homeworks.zadanie1;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 public class GuessNumberGame {
+
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 10;
+    private int playerNumberGuess = 0;
+    private boolean hasWon = false;
+    private int numberToGuess = (int) (Math.random() * MAX_VALUE + 1);
+
     public String getIntro() {
-        throw new NotImplementedException("method getIntro() is not implemented");
+        return
+                """
+                        !!!! The Best Guess Number Game Ever !!!!!
+                         Zasady gry:
+                         1. Wybierz tryb gry wpisująć PLAYER 
+                         2. Wybierz tryb gry STANDARD wpisująć dowolna inna wartość niż PLAYER
+                         2. STANDARD - gra będzie miała ustawiony przedział, limit szans jest nieograniczony
+                         3. PLAYER - gracz ma możliość ustalenia:
+                                - przedziału liczb;
+                                - ilość możliwości zgadywnia; 
+                        Wybierz rodzaj rozgrywki i ciesz się zabawą ;)
+                        """;
     }
 
     public boolean isRunning() {
-        throw new NotImplementedException("method isRunning() is not implemented");
+        if (hasWon) {
+            return false;
+        }
+        return true;
     }
 
     public String getInstruction() {
-        throw new NotImplementedException("method getInstruction() is not implemented");
+        return "Podaj liczbę z przedziału " + MIN_VALUE + " - " + MAX_VALUE;
+    }
+
+
+    public String getPlayerInstruction(int min, int max) {
+        return "Podaj liczbę z przedziału " + min + " - " + max;
     }
 
     public String guessNumber(int number) {
-        throw new NotImplementedException("method guessNumber() is not implemented");
+        if (number > numberToGuess) {
+            return "Szukana liczba jest mniejsza niż :" + number;
+        }
+        if (number < numberToGuess) {
+            return "Szukana liczba jest większa niż :" + number;
+        }
+        hasWon = true;
+        return "WYGRAŁEŚ";
+    }
+
+
+    public String guessPlayerNumber(int number, int numberOfChances) {
+        if (numberOfChances > 0) {
+            if (number > playerNumberGuess) {
+                return "Szukana liczba jest mniejsza niż :" + number;
+            }
+            if (number < playerNumberGuess) {
+                return "Szukana liczba jest większa niż :" + number;
+            }
+            if (number == playerNumberGuess) {
+                hasWon = true;
+                return "WYGRAŁES";
+            }
+        }
+        hasWon = true;
+        return "PORAŻKA";
+    }
+
+    public void playerNumberToGuess(int min, int max) {
+        playerNumberGuess = (int) (min + (Math.random() * (max - min + 1)));
     }
 }
